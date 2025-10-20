27e43d40bceeb82ed09a9f5d1677746589aabfca
Lennon Porto <lennonporto@gmail.com>
2025-10-20 20:43:36 -0300

    simplify repository

--- src/main/java/com/example/demo/repository/IslandRepository.java
+++ src/main/java/com/example/demo/repository/IslandRepository.java
@@ -8,18 +8,14 @@ import org.springframework.data.repository.ListCrudRepository;
 import com.example.demo.repository.entity.Island;
 import com.example.demo.repository.entity.Island.Disposition;
 
-public interface IslandRepository 
-    extends ListCrudRepository<Island, Long> {
+public interface IslandRepository extends ListCrudRepository<Island, Long> {
 
     List<Island> findByDisposition(Disposition disposition);
- 
-    /*
-     * GROUP BY w
-     * HAVING w.user IS NULL
-     */
+
     @Query(value = """
-            SELECT i 
-            FROM Island i JOIN i.workstations w
+            SELECT i
+            FROM Island i
+            JOIN i.workstations w
             WHERE w.user IS NULL
             """)
     List<Island> findIslandWithAvailableWorkstations();
