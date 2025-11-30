029e40562a4bad41096fb03839553f45331feace
Lennon Porto <lennonporto@gmail.com>
2025-11-30 20:46:06 -0300

    ?

--- bin/main/schema.sql
+++ bin/main/schema.sql
@@ -26,3 +26,28 @@ CREATE TABLE IF NOT EXISTS profiles (
     type VARCHAR(255),
     FOREIGN KEY (id) REFERENCES users(id)
 );
+
+CREATE TABLE IF NOT EXISTS tickets
+(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    creator_id  INT          NOT NULL,
+    assignee_id INT,
+    object      VARCHAR(100) NOT NULL,
+    action      VARCHAR(100) NOT NULL,
+    details     VARCHAR(100) NOT NULL,
+    locality    VARCHAR(100) NOT NULL,
+    status      VARCHAR(50) NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (creator_id) REFERENCES users (id),
+    FOREIGN KEY (assignee_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS tickets_observers
+(
+    ticket_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (ticket_id, user_id), 
+    FOREIGN KEY (ticket_id) REFERENCES tickets (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
